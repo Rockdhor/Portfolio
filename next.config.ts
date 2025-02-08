c10a0214f72436ce6cb190b28340874dd8108227
Allen Schmerler <allenschmerler@gmail.com>
2025-02-08 18:06:34 -0500

    changed some more configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Portfolio/',
+  assetPrefix: '/Portfolio/',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
-
 import "./globals.css";
 
 export const metadata: Metadata = {
